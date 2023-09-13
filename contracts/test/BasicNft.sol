9246ec9df0209f3cd97391975720c42f8340eeda
Tarun Rao <tarunsanjayrao@gmail.com>
2023-09-14 00:50:59 +0530

    NFT MarketPlace

--- contracts/test/BasicNft.sol
+++ contracts/test/BasicNft.sol
@@ -5,18 +5,18 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNft is ERC721 {
     string public constant TOKEN_URI =
-        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+        "https://ipfs.io/ipfs/Qme2LChChpf6LZV3cqXCB5GbbdDjqGzUXynRguarMt865v?filename=Travis.json";
     uint256 private s_tokenCounter;
 
-    event DogMinted(uint256 indexed tokenId);
+    event NFTMinted(uint256 indexed tokenId);
 
-    constructor() ERC721("Dogie", "DOG") {
+    constructor() ERC721("TRAVIS", "RAP") {
         s_tokenCounter = 0;
     }
 
     function mintNft() public {
         _safeMint(msg.sender, s_tokenCounter);
-        emit DogMinted(s_tokenCounter);
+        emit NFTMinted(s_tokenCounter);
         s_tokenCounter = s_tokenCounter + 1;
     }
 
